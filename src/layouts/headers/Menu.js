bd1db95a2bb891a34229c19fbeead4defd43bef0
TaalibAsaf <137051454+TaalibAsaf@users.noreply.github.com>
2024-05-23 22:41:35 +0530

    first commit

---
+++ src/layouts/headers/Menu.js
@@ -0,0 +1,69 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
+export const Home = () => (
+  <Fragment>
+    <li>
+      <Link href="/index-four">Home Four</Link>
+    </li>
+  </Fragment>
+);
+export const Pages = () => (
+  <Fragment>
+    <li>
+      <Link href="/pricing">Pricing Plan</Link>
+    </li>
+    <li>
+      <Link href="/faq">Help &amp; FAQ</Link>
+    </li>
+    <li>
+      <Link href="/gallery">Our Gallery</Link>
+    </li>
+    <li>
+      <Link href="/appointments">Appointment</Link>
+    </li>
+  </Fragment>
+);
+export const Services = () => (
+  <Fragment>
+    <li>
+      <Link href="/service">Service One</Link>
+    </li>
+    <li>
+      <Link href="/service-two">Service Two</Link>
+    </li>
+    <li>
+      <Link href="/service-details">Service Details</Link>
+    </li>
+  </Fragment>
+);
+export const Doctor = () => (
+  <Fragment>
+    <li>
+      <Link href="/doctor">Doctor</Link>
+    </li>
+    <li>
+      <Link href="/doctor-details">Doctor Details</Link>
+    </li>
+  </Fragment>
+);
+export const Blog = () => (
+  <Fragment>
+    <li>
+      <Link href="/blog-standard">Blog Standard</Link>
+    </li>
+    <li>
+      <Link href="/blog-details">Blog Details</Link>
+    </li>
+  </Fragment>
+);
+export const Shop = () => (
+  <Fragment>
+    <li>
+      <Link href="/shop">Shop</Link>
+    </li>
+    <li>
+      <Link href="/product-details">Shop Details</Link>
+    </li>
+  </Fragment>
+);
